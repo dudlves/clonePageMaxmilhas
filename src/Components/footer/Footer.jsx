96ca6e5edec3c44182900fd125639bd494fbfce4
dudlves <dudlves17@gmail.com>
2025-05-15 07:45:10 -0300

    feat: added about-footer

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -19,6 +19,40 @@ function Footer(){
         <p>Promoção</p>
         <p id='invisible-menu'>Mais Opções</p>
       </div>
+      <div className='linha'></div>
+      <div id='about-footer'>
+        <div>
+          <h5>A Maxmilhas</h5>
+          <p>Quem somos</p>
+          <p>Blog</p>
+          <p>Imprensa</p>
+          <p>Trabalhe conosco</p>
+          <p>Termos e condições</p>
+          <p>Política de privacidade</p>
+          <p>Lei de Igualdade Salarial - publicado em 23/09/2024</p>
+        </div>
+        <div>
+          <h5>Atendimento 24h</h5>
+          <p>Dúvidas frequentes</p>
+          <p>Remarcações/Cancelamentos</p>
+          <p>Fale conosco</p>
+        </div>
+        <div>
+          <h5>Forma de pagamento</h5>
+          <p>Cartões de crédito</p>
+          <p>Transferência via PIX</p>
+        </div>
+        <div>
+          <h5>Redes sociais</h5>
+          <p>Facebook</p>
+          <p>Instagram</p>
+          <p>Twitter</p>
+          <p>LinkedIn</p>
+          <p>YouTube</p>
+        </div>
+      </div>
+      <div className='linha'></div>
+
 
     </footer>
   )
